ef039fee2cf4f09c880148cd7fda891ee0d991f0
OlegMorev <proks200413@gmail.com>
2025-03-27 06:22:05 +0300

    Good Anchor

---
+++ ARSezriv/Assets/OMorev/Scripts/AnchoringManager.cs
@@ -0,0 +1,30 @@
+using Meta.XR.BuildingBlocks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchoringManager : MonoBehaviour
+{
+    public SpatialAnchorSpawnerBuildingBlock spatialAnchorSpawnerBuildingBlock;
+    public ControllerButtonsMapper buttonsMapper;
+    public SpatialAnchorLoaderBuildingBlock loader;
+    private PhantomInstance OnHandAnchor;
+    public void SetNewAnchor()
+    {
+        FindObjectOfType<SpatialAnchorCoreBuildingBlock>().EraseAllAnchors();
+        spatialAnchorSpawnerBuildingBlock.gameObject.SetActive(true);
+        buttonsMapper.gameObject.SetActive(true);
+        if(OnHandAnchor != null) OnHandAnchor.gameObject.SetActive(true);
+    }
+    public void LoadAnchor()
+    {
+        loader.LoadAnchorsFromDefaultLocalStorage();
+    }
+
+    public void SetOnHandAnchor(PhantomInstance onHand)
+    {
+        OnHandAnchor = onHand;
+        OnHandAnchor.gameObject.SetActive(false);
+        Debug.Log("FUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK");
+    }
+}
---
+++ ARSezriv/Assets/OMorev/Scripts/PhantomInstance.cs
@@ -0,0 +1,28 @@
+using Meta.XR.BuildingBlocks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhantomInstance : MonoBehaviour
+{
+    AnchoringManager anchoringManager;
+    private void Start()
+    {
+        anchoringManager = FindObjectOfType<AnchoringManager>();
+        if(transform.parent == null)
+        {
+            anchoringManager.spatialAnchorSpawnerBuildingBlock.gameObject.SetActive(false);
+            anchoringManager.buttonsMapper.gameObject.SetActive(false);
+            foreach(PhantomInstance phantom in FindObjectsOfType<PhantomInstance>())
+            {
+                if(phantom != this)
+                {
+                    anchoringManager.SetOnHandAnchor(phantom);
+                }
+            }
+        }
+    }
+
+
+    
+}
